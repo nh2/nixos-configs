6f9ab4002ca5d9f71c25321f90e90d6ea45b7a69
Niklas Hambüchen <mail@nh2.me>
2024-05-03 19:44:53 +0200

    Increase ThinkPad trackpoint sensitivity

--- configuration.nix
+++ configuration.nix
@@ -598,6 +598,28 @@ in
     environment.enableDebugInfo = true;
 
     hardware.sane.enable = true; # enables support for SANE scanners
+    hardware.sane.backends-package = pkgs.sane-backends.overrideAttrs (old: {
+      configureFlags = (old.configureFlags or []) ++ [
+        # "--localstatedir=/var" # `sane-backends` puts e.g. lock files in here, must not be in /nix/store
+        # "--with-lockdir=/var/lock/sane" # `sane-backends` puts e.g. lock files in here, must not be in /nix/store
+
+        # Ugly workaround for https://github.com/NixOS/nixpkgs/issues/273280#issuecomment-1848873028
+        # Really we should make `sane-backends` be able to provide a real lock dir (e.g. `/var/lock/sane`).
+        "--disable-locking"
+      ];
+      # Alternative workaround for https://github.com/NixOS/nixpkgs/issues/273280#issuecomment-1848873028
+      # We'd prefer to just set in `configureFlags`
+      #     "--localstatedir=/var" # `sane-backends` puts e.g. lock files in here, must not be in /nix/store
+      # but that does not work because the install step tries to create this directory,
+      # which fails in the nix build sandbox.
+      # So instead, we set the preprocessor variable directly, see:
+      #     https://gitlab.com/sane-project/backends/-/blob/65779d6b595547d155a1954958bce5faaad45a5d/configure.ac#L635-652
+      # A problem is that this lock dir also needs to exist and have write permissions.
+      # Right now you have to do that manually with:
+      #     sudo mkdir -p /var/lock/sane && sudo chown root:scanner /var/lock/sane && sudo chmod g+w /var/lock/sane
+      # Maybe we should use the `scanner` group for that, and/or configure it with systemd `tmpfiles`.
+      #NIX_CFLAGS_COMPILE = "-DPATH_SANE_LOCK_DIR=/var/lock/sane";
+    });
 
     # Steam needs this, see https://nixos.org/nixpkgs/manual/#sec-steam-play
     hardware.opengl.driSupport32Bit = true;
@@ -924,6 +946,13 @@ in
     system.stateVersion = "19.09"; # Did you read the comment?
 
     # services.keybase.enable = true;
+
+    hardware.trackpoint = {
+      enable = true;
+      # Documentation of the options is at:
+      #     https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-devices-platform-trackpoint
+      sensitivity = 215; # default is too slow for me; 215 seems to be the max
+    };
 
     # xinput to set my preferred scroll behaviour for the Tex Shinobi,
     # via `xorg.conf` so that it also applies when re-plugged.
